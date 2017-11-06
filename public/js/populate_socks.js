56d7faee157068ef131434ae4b33c8793a0adb96
Live Sound Systems <hillaryellis37@gmail.com>
2017-11-05 18:37:30 -0800

    owner specific socks now populate in modal

--- public/js/populate_socks.js
+++ public/js/populate_socks.js
@@ -1,6 +1,6 @@
 (function(){
 	//dummy pictures
-var ownerId = 1;
+var ownerId = "1";
 
 
 //sock html template 
@@ -13,10 +13,24 @@ var	sockSrc =
 		"</div>",
 		"<h3 class='name'>{{name}}</h3>",
 		"<p class='description'>{{description}}</p>",
+		"<div class='trade' data-toggle='modal' data-target='#myModal-trade'> Trade Sock > </div>",
 	"</div>",
 "</div></a>",
 ].join("");
 
+var ownerSock = 
+["<div class='photo-holder'>",
+	"<img class='my-socks' src='{{mySockImg}}'> ",
+ "</div>"	
+].join("");
+
+function getMySockHtml(mySockImg) {
+	var template = Handlebars.compile(ownerSock);
+	var context = {
+		mySockImg: mySockImg,
+	};
+	return template(context);
+}
 
 function getSockHtml(name, description, sockImg, profile_img){
 	var template = Handlebars.compile(sockSrc);
@@ -28,6 +42,20 @@ function getSockHtml(name, description, sockImg, profile_img){
 		// Owner: user_name
 	};
 	return template(context);
+}
+
+
+function renderMySocks(){
+	$.ajax({
+		method: "GET",
+		url: "/api/socks/" + ownerId
+	}).done(function(sockArr){
+
+		sockArr.forEach(function(sock){
+			var sockDiv = $(getMySockHtml(sock.image_path));
+			$(".modal-sock-pix")[0].append(sockDiv[0]);
+		})
+	})
 }
 
 function renderSocks(){
@@ -50,7 +78,8 @@ function renderSocks(){
 
 //on page load render socks
 $(document).ready(function(){
-	renderSocks()
+	renderSocks();
+	renderMySocks();
 })
 
 //if we need renderSocks in global scope

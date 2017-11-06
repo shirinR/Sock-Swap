c1b8e5635d9d7c0d562f67be138e73e4156dc3b2
shirinR <shirin.raad@gmail.com>
2017-11-05 17:24:54 -0800

    edit

--- public/js/signin.js
+++ public/js/signin.js
@@ -2,12 +2,12 @@ $(function() {
 
   $('#btn_login').on('click', function(event){
     var userInfo = {
-      user_name: $('#username').val().trim(),
+      username: $('#username').val().trim(),
       password: $('#password').val().trim()
     };
     console.log('here');
 
-    if(user_name === owner.user_name && password === owner.password){
+    if(username === Owner.user_name && password === Owner.password){
       // $.get('/main', userInfo).done(...);
     }
   });
